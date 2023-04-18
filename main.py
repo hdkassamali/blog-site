23f956b3aecd4efc87131eb2c1c0a3108fbee62d
hdkassamali <122648642+hdkassamali@users.noreply.github.com>
2023-04-18 14:52:40 -0700

    added functionality for contact-me.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import requests
+import smtplib
+import os
+gmail_user = os.environ.get("gmail_user")
+gmail_pass = os.environ.get("gmail_pass")
 
 app = Flask(__name__)
 
@@ -16,9 +20,20 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact-me")
-def contact_me():
-    return render_template("contact.html")
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(gmail_user, gmail_pass)
+        connection.sendmail(gmail_user, gmail_user, email_message)
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -27,6 +42,7 @@ def show_post(index):
         if post["id"] == index:
             requested_post = post
     return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
